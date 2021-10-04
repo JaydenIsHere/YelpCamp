8cd444a616e21cdfd994395e9cba4f8672f84be2
unknown <classic19931126@gmail.com>
2021-10-04 14:20:47 +0800

    add start script

--- app.js
+++ app.js
@@ -163,8 +163,9 @@ app.use((err,req,res,next) =>{//here is handle majority of error includeing when
     res.status(statusCode).render('error',{ err } );
 })
 
-app.listen(3000, () =>{
-    console.log('We are listen on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () =>{
+    console.log(`We are listen on port ${port}`)
 })
 
 
